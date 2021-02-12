c89eb999bbabc02b977109c0764381951cfb631c
Christian Kreibich <christian@corelight.com>
2021-02-11 18:02:13 -0800

    A bunch of work on plugin support -- works! With tests!

--- zeekpkg/templates/plugin/plugin/src/Plugin.cc
+++ zeekpkg/templates/plugin/plugin/src/Plugin.cc
@@ -1,4 +1,4 @@
-
+#include "config.h"
 #include "Plugin.h"
 
 namespace plugin { namespace @PACKAGE_NS_UNDERSCORE@@PACKAGE_NAME@ { Plugin plugin; } }
@@ -8,10 +8,10 @@ using namespace plugin::@PACKAGE_NS_UNDERSCORE@@PACKAGE_NAME@;
 zeek::plugin::Configuration Plugin::Configure()
 	{
 	zeek::plugin::Configuration config;
-	config.name = "@PACKAGE_NS_COLONS@@PACKAGE_NAME@";
+	config.name = "@PACKAGE_NS@::@PACKAGE_NAME@";
 	config.description = "TODO: Insert description";
-	config.version.major = 0;
-	config.version.minor = 1;
-	config.version.patch = 0;
+	config.version.major = PACKAGE_VERSION_MAJOR;
+	config.version.minor = PACKAGE_VERSION_MINOR;
+	config.version.patch = PACKAGE_VERSION_PATCH;
 	return config;
 	}
--- zeekpkg/templates/plugin/testing/@PACKAGE_SLUG@/show-plugin.zeek
+++ zeekpkg/templates/plugin/testing/@PACKAGE_SLUG@/show-plugin.zeek
@@ -1,2 +1,2 @@
-# @TEST-EXEC: zeek -NN @PACKAGE_NS_COLONS@@PACKAGE_NAME@ |sed -e 's/version.*)/version)/g' >output
+# @TEST-EXEC: zeek -NN @PACKAGE_NS@::@PACKAGE_NAME@ |sed -e 's/version.*)/version)/g' >output
 # @TEST-EXEC: btest-diff output
